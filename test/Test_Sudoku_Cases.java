e175a42d47830bc6a30a16c5147a43ca2fa65099
Anna Ira <annairahurnaus@mytum.de>
2018-07-19 10:42:24 +0200

    small test fixes

--- test/Test_Sudoku_Cases.java
+++ test/Test_Sudoku_Cases.java
@@ -6,6 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Test_Sudoku_Cases {
 
     @Test
+    public void test_hardest_sudoku() throws Exception {
+
+        // https://www.telegraph.co.uk/news/science/science-news/9359579/Worlds-hardest-sudoku-can-you-crack-it.html
+        String puzzle = "800_000_000_003_600_000_070_090_200_050_007_000_000_045_700_000_100_030_001_000_068_008_500_010_090_000_400";
+        String solution = "812753649943682175675491283154237896369845721287169534521974368438526917796318452";
+        assertEquals(solution, new Sudoku(puzzle).solve(), new Sudoku(puzzle).toURL());
+    }
+
+    @Test
     public void test_0() throws Exception {
 
         String puzzle_0 = "048_009_531_716_005_902_005_008_000_000_000_703_302_960_018_497_000_000_000_001_367_003_820_009_069_700_005";
--- test/Test_Sudoku_Not_Solvable_Cases.java
+++ test/Test_Sudoku_Not_Solvable_Cases.java
@@ -13,23 +13,15 @@ public class Test_Sudoku_Not_Solvable_Cases {
             fail("Exception expected.");
         } catch (SudokuException e) {
             assertEquals(e.getMessage(), "Not eindeutig \n" +
-                    "926571483git sta351486279874923516582367194149258367763149825238794651617835942495612738\n" +
+                    "926571483351486279874923516582367194149258367763149825238794651617835942495612738\n" +
                     "926571483351486279874923516582367194149258367763194825238749651617835942495612738");
         }
     }
 
+
+
     @Test
     public void test_2() throws Exception {
-
-        // https://www.telegraph.co.uk/news/science/science-news/9359579/Worlds-hardest-sudoku-can-you-crack-it.html
-        String puzzle = "800_000_000_003_600_000_070_090_200_050_007_000_000_045_700_000_100_030_001_000_068_008_500_010_090_000_400";
-        String solution = "812753649943682175675491283154237896369845721287169534521974368438526917796318452";
-        assertEquals(solution, new Sudoku(puzzle).solve(), new Sudoku(puzzle).toURL());
-    }
-
-    @Test
-    public void test_3() throws Exception {
-        // https://www.telegraph.co.uk/news/science/science-news/9359579/Worlds-hardest-sudoku-can-you-crack-it.html
         String puzzle = "070_006_000_900_000_041_008_009_050_090_007_002_003_000_800_400_800_010_080_300_900_160_000_007_000_500_080";
         try {
             new Sudoku(puzzle).solve();
